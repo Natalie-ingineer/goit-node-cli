e2dd3838dc0f9873a947f007428960c828ac6a69
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-03-23 18:12:04 +0200

    add path

--- contacts.js
+++ contacts.js
@@ -2,9 +2,16 @@ import { promises as fs } from "fs";
 import path from "path";
 
 const contactsPath = path.join("db", "contacts.json");
-console.log({ contactsPath });
+// console.log({ contactsPath });
 
-async function listContacts() {}
+async function listContacts() {
+  const readContacts = await fs.readFile(contactsPath);
+  const contactOject = JSON.parse(readContacts);
+  return contactOject;
+}
+
+const renderContacts = () => {};
+
 async function getContactById(contactId) {}
 async function removeContact(contactId) {}
 async function addContact(name, email, phone) {}
