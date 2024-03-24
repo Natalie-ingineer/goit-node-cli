b36c70cf283a24eaf7d5ce41dae3d5a7b410e5ac
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-03-24 17:52:00 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -3,66 +3,79 @@ const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 
 const contactsPath = path.join("db", "contacts.json");
-// console.log({ contactsPath });
 
 async function listContacts() {
-  const readContacts = await fs.readFile(contactsPath);
-  const contactOject = JSON.parse(readContacts);
-  return contactOject;
+  try {
+    const readContacts = await fs.readFile(contactsPath);
+    const contactOject = JSON.parse(readContacts);
+    return contactOject;
+  } catch (error) {
+    console.error(message.error);
+    return [];
+  }
 }
-
-// const renderContacts = () => {};
 
 async function getContactById(contactId) {
-  const readContacts = await fs.readFile(contactsPath);
-  const contactOject = JSON.parse(readContacts);
-  const contactById = contactOject.find((item) => item.id === contactId);
+  try {
+    const readContacts = await fs.readFile(contactsPath);
+    const contactOject = JSON.parse(readContacts);
+    const contactById = contactOject.find((item) => item.id === contactId);
 
-  return contactById || null;
-  // const getContact = await fs.readFile(listContacts());
-  // return getContact.map((item) => item.id);
+    return contactById || null;
+  } catch (error) {
+    console.error(message.error);
+    return null;
+  }
 }
 
-// getContactById("qdggE76Jtbfd9eWJHrssH").then((result) => console.log(result));
-
 async function removeContact(contactId) {
-  const readContacts = await fs.readFile(contactsPath);
-  const contactOject = JSON.parse(readContacts);
-  const contactById = contactOject.find((item) => item.id === contactId);
-  if (contactById !== -1) {
-    const removeContact = contactOject.splice(contactById, 1);
-    await fs.writeFile(
-      contactsPath,
-      JSON.stringify(contactOject, null, 2),
-      "utf8"
-    );
+  try {
+    const readContacts = await fs.readFile(contactsPath);
+    const contactOject = JSON.parse(readContacts);
+    const contactById = contactOject.find((item) => item.id === contactId);
+    if (contactById !== -1) {
+      const removeContact = contactOject.splice(contactById, 1);
+      await fs.writeFile(
+        contactsPath,
+        JSON.stringify(contactOject, null, 2),
+        "utf8"
+      );
 
-    return removeContact;
+      return removeContact;
+    }
+
+    return null;
+  } catch (error) {
+    console.error(message.error);
+    return null;
   }
-  // Якщо контакт не знайдено, повертаємо null
-  return null;
 }
 
 removeContact("qdggE76Jtbfd9eWJHrssH").then((result) => console.log(result));
 
 async function addContact(name, email, phone) {
-  const readContacts = await fs.readFile(contactsPath);
-  const contactOject = JSON.parse(readContacts);
+  try {
+    const readContacts = await fs.readFile(contactsPath);
+    const contactOject = JSON.parse(readContacts);
 
-  const newContact = {
-    id: uuidv4(),
-    name,
-    email,
-    phone,
-  };
-  contactOject.push(newContact);
+    const newContact = {
+      id: uuidv4(),
+      name,
+      email,
+      phone,
+    };
+    contactOject.push(newContact);
 
-  await fs.writeFile(
-    contactsPath,
-    JSON.stringify(contactOject, null, 2),
-    "utf8"
-  );
-  return newContact;
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(contactOject, null, 2),
+      "utf8"
+    );
+    return newContact;
+  } catch (error) {
+    console.error(message.error);
+    return null;
+  }
 }
 
 addContact("Anna", "anna@gerz.com", "(508) 252-5118)").then((result) =>
